1279bc7f51303883bf985a947725977e0fc09b75
Lucy <has21013@byui.edu>
2024-04-29 23:35:30 -0600

    prep2-4

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+        string letter;
+
+        if (gradePercentage >= 90)
+            letter = "A";
+        else if (gradePercentage >= 80)
+            letter = "B";
+        else if (gradePercentage >= 70)
+            letter = "C";
+        else if (gradePercentage >= 60)
+            letter = "D";
+        else
+            letter = "F";
+
+        bool passed = gradePercentage >= 70;
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (passed)
+            Console.WriteLine("Congratulations, you passed the course!");
+        else
+            Console.WriteLine("Don't worry, keep working hard for next time!");
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); // Generate a random number between 1 and 100
+
+        int guess;
+        bool guessedCorrectly = false;
+        int guessesCount = 0;
+
+        Console.WriteLine("Guess My Number Game!");
+        Console.WriteLine("I've picked a number between 1 and 100. Try to guess it.");
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            guessesCount++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                guessedCorrectly = true;
+            }
+        } while (!guessedCorrectly);
+
+        Console.WriteLine($"It took you {guessesCount} guesses to find the magic number.");
     }
 }
